e19c594e9c2ef47af03361691bb226b565bc4109
Stefan Boos <kontakt@boos.systems>
2023-11-05 20:32:34 +0100

    feat: return [] when first parameter is []

--- tests/arrayDiff.test.ts
+++ tests/arrayDiff.test.ts
@@ -2,8 +2,18 @@ import { expect } from 'chai';
 
 import { arrayDiff } from '../src/arrayDiff';
 
+describe('arrayDiff should', () => {
+    it('return [] when a is []', () => {
+        expect(arrayDiff([], [])).to.eql([]);
+    });
+
+    xit('return a when b is []', () => {
+        expect(arrayDiff([1], [])).to.eql([1]);
+    });
+});
+
 describe('Basic tests', () => {
-    it('Basic test should work', () => {
+    xit('Basic test should work', () => {
         expect(arrayDiff([], [4, 5])).to.eql([], 'a was [], b was [4,5]');
         expect(arrayDiff([3, 4], [3])).to.eql([4], 'a was [3, 4], b was [3]');
         expect(arrayDiff([1, 8, 2], [])).to.eql([1, 8, 2], 'a was [1, 8, 2], b was []');
