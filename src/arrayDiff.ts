8b0f7618f0496715804d146c7ce087e060f15b40
Stefan Boos <kontakt@boos.systems>
2023-11-08 05:15:27 +0100

    refactor: replace loop by filter

--- src/arrayDiff.ts
+++ src/arrayDiff.ts
@@ -1,11 +1,3 @@
 export function arrayDiff(a: number[], b: number[]): number[] {
-    let result: number[] = []
-
-    a.forEach(value => {
-        if (!b.includes(value)) {
-            result.push(value);
-        }
-    })
-
-    return result;
+    return a.filter(value => !b.includes(value));
 }
