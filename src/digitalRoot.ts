4824c7e3d275c4ab0d84a88ab27d4ac4dcac6f8e
Stefan Boos <kontakt@boos.systems>
2023-11-09 05:34:41 +0100

    feat: digitalRoot returns plain sum of digits when sum < 10

--- src/digitalRoot.ts
+++ src/digitalRoot.ts
@@ -1,3 +1,15 @@
-export const digitalRoot = (n:number):number => {
-    return n;
+export const digitalRoot = (n: number): number => {
+  let sum = [...Array(numberOfDigits()).keys()]
+    .map(digitAt)
+    .reduce((sum, digit) => sum + digit, 0);
+
+  return sum;
+
+  function numberOfDigits(): number {
+    return Math.floor(Math.log10(n)) + 1;
+  }
+
+  function digitAt(decimalPlace: number): number {
+    return Math.floor(n / Math.pow(10, decimalPlace)) % 10;
+  }
 };
--- tests/digitalRoot.test.ts
+++ tests/digitalRoot.test.ts
@@ -3,8 +3,19 @@ import { assert } from "chai";
 import { digitalRoot } from "../src/digitalRoot";
 
 describe("digitalRoot should", () => {
-  it.each([0, 1, 2])("return n when n is %p", (n: number) => {
-    digitalRoot(1).should.equal(1);
+  describe("return n when n < 10", () => {
+    it.each([0, 1, 2, 9])("n = %p", (n: number) => {
+      digitalRoot(1).should.equal(1);
+    });
+  });
+
+  describe("return plain sum of digits when sum < 10", () => {
+    it.each([[1, 10]])(
+      "expect %p for n = %p",
+      (expected: number, n: number) => {
+        digitalRoot(n).should.equal(expected);
+      }
+    );
   });
 });
 
