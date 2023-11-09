4cf909323fc9b6e94d85bf298e1e90efdd061c4c
Stefan Boos <kontakt@boos.systems>
2023-11-09 05:15:00 +0100

    feat: digitalRoot returns n when n < 10

--- src/digitalRoot.ts
+++ src/digitalRoot.ts
@@ -1,3 +1,3 @@
 export const digitalRoot = (n:number):number => {
-    return 0;
+    return n;
 };
--- tests/digitalRoot.test.ts
+++ tests/digitalRoot.test.ts
@@ -1,16 +1,16 @@
-import 'chai/register-should';
-import {assert} from 'chai';
-import {digitalRoot} from '../src/digitalRoot';
+import "chai/register-should";
+import { assert } from "chai";
+import { digitalRoot } from "../src/digitalRoot";
 
-describe('digitalRoot should', () => {
-    it('return 0 when n is 0', () => {
-        digitalRoot(0).should.equal(0);
-    });
+describe("digitalRoot should", () => {
+  it.each([0, 1, 2])("return n when n is %p", (n: number) => {
+    digitalRoot(1).should.equal(1);
+  });
 });
 
 describe("solution", () => {
-    xit('should work for fixed tests', () => {
-        assert.equal(digitalRoot(16), 7);
-        assert.equal(digitalRoot(456), 6 );
-    });
+  xit("should work for fixed tests", () => {
+    assert.equal(digitalRoot(16), 7);
+    assert.equal(digitalRoot(456), 6);
+  });
 });
