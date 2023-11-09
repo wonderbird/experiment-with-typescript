14e18db6ea2ea5d9d0b5264c7d1a6c2f730c3f1f
Stefan Boos <kontakt@boos.systems>
2023-11-09 05:44:25 +0100

    refactor: shorten method bodies

--- src/digitalRoot.ts
+++ src/digitalRoot.ts
@@ -1,21 +1,15 @@
 export const digitalRoot = (n: number): number => {
-  let sum = [...Array(numberOfDigits()).keys()].map(digitAt).reduce(add, 0);
+  let numberOfDigits = Math.floor(Math.log10(n)) + 1;
 
+  let digitAt = (decimalPlace: number) =>
+    Math.floor(n / Math.pow(10, decimalPlace)) % 10;
+
+  let add = (a: number, b: number) => a + b;
+
+  let sum = [...Array(numberOfDigits).keys()].map(digitAt).reduce(add, 0);
   if (sum >= 10) {
     return digitalRoot(sum);
   }
 
   return sum;
-
-  function numberOfDigits(): number {
-    return Math.floor(Math.log10(n)) + 1;
-  }
-
-  function digitAt(decimalPlace: number): number {
-    return Math.floor(n / Math.pow(10, decimalPlace)) % 10;
-  }
-
-  function add(a: number, b: number) {
-    return a + b;
-  }
 };
