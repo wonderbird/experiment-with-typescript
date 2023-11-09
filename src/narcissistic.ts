7287cbe46f58b6b2a07a2ab1968c79ae684709a9
Stefan Boos <kontakt@boos.systems>
2023-11-09 21:20:32 +0100

    feat: narcissistic kata

--- src/narcissistic.ts
+++ src/narcissistic.ts
@@ -1,3 +1,8 @@
 export function narcissistic(value: number): boolean {
-  return false;
+  const digits = value.toString().split("");
+  const power = digits.length;
+  const sum = digits
+    .map(Number)
+    .reduce((acc, digit) => acc + Math.pow(digit, power), 0);
+  return value === sum;
 }
--- tests/narcissistic.test.ts
+++ tests/narcissistic.test.ts
@@ -1,6 +1,37 @@
+import "chai/register-should";
 import { expect } from "chai";
-
 import { narcissistic } from "../src/narcissistic";
+
+describe("narcissistic should", () => {
+  // My understanding of the problem
+  // ---
+  // 1, 2, 3 ..., 9 -> true
+  // 10, 11, 12, ..., 99 -> false
+  // 153 -> 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153 -> true
+  // ---
+  // For each digit, raise it to the power of the number of digits
+  // Sum the results
+  // If the sum equals the original number, return true
+  // Otherwise, return false
+
+  describe("return true when n < 10", () => {
+    it.each([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])("n = %p", (n) => {
+      narcissistic(n).should.equal(true);
+    });
+  });
+
+  describe("return true for narcissistic numers > 10", () => {
+    it.each([153, 1634])("n = %p", (n) => {
+      narcissistic(n).should.equal(true);
+    });
+  });
+
+  describe("return false for non-narcissistic numbers", () => {
+    it.each([10])("n = %p", (n) => {
+      narcissistic(n).should.equal(false);
+    });
+  });
+});
 
 describe("Basic tests", () => {
   it("Basic test should work", () => {
