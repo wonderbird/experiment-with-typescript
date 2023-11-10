81329bdb39c13577f13989fa59aaac4145410ec8
Stefan Boos <kontakt@boos.systems>
2023-11-10 05:35:22 +0100

    docs: problem understanding

--- src/clock.ts
+++ src/clock.ts
@@ -1,3 +1,22 @@
+// My understanding of the problem:
+//
+// n: maximum number of pages that can be stored in memory at the same time
+// referenceList: IDs of the pages requested one after the other
+//
+// returns: status of memory after application of the algorithm
+//
+// test cases:
+//
+// | n | referenceList     | expected |
+// |---|-------------------|----------|
+// | 0 | any               | []       |
+// | 1 | []                | [-1]     |
+// | 1 | any               | [last element in referenceList] |
+// | 2 | []                | [-1, -1] |
+// | 2 | any one element   | [that element, -1] |
+// | 2 | any number of same elements  | [that element, -1] |
+// | 2 | any two different elements  | [first, second] |
+// | 2 | two different elements alternating  | depends on sequence |
 export function clock(n: number, referenceList: number[]): number[] {
   return [];
 }
