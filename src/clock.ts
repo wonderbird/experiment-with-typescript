fc712ea59fdbbf1dedebb5995a3bdc8a011c7052
Stefan Boos <kontakt@boos.systems>
2023-11-17 23:08:12 +0100

    refactor: moving classes out of function

--- src/clock.ts
+++ src/clock.ts
@@ -1,84 +1,65 @@
-// My understanding of the problem:
-//
-// n: maximum number of pages that can be stored in memory at the same time
-// referenceList: IDs of the pages requested one after the other
-//
-// returns: status of memory after application of the algorithm
-//
-// test cases:
-//
-// | n | referenceList     | expected |
-// |---|-------------------|----------|
-// | 0 | any               | []       |
-// | 1 | []                | [-1]     |
-// | 2 | []                | [-1, -1] |
-// | 2 | any one element   | [that element, -1] |
-// | 2 | any two different elements  | [first, second] |
-// | 1 | any               | [last element in referenceList] |
-// | 2 | any number of same elements  | [that element, -1] |
-// | 2 | two different elements alternating  | depends on sequence |
+class Page {
+  constructor(public id: number, public referenceCounter: number) {}
+}
+
+class Memory {
+  private readonly pages: Page[] = [];
+  private iterator: number = 0;
+
+  constructor(n: number) {
+    this.pages = Array(n).fill(new Page(-1, 0));
+  }
+
+  public get pageIds(): number[] {
+    return this.pages.map((page) => page.id);
+  }
+
+  public request(pageReference: number) {
+    if (this.contains(pageReference)) {
+      this.increment(pageReference);
+    } else {
+      this.findInsertPosition();
+      this.insert(pageReference);
+    }
+  }
+
+  private contains(pageReference: number) {
+    return this.pages.some((page) => page.id === pageReference);
+  }
+
+  private increment(pageReference: number) {
+    let page = this.pages.find((page) => page.id === pageReference)!;
+    page.referenceCounter++;
+  }
+
+  private findInsertPosition() {
+    if (this.currentPage().referenceCounter > 0) {
+      this.currentPage().referenceCounter--;
+      this.advanceIterator();
+      this.findInsertPosition();
+    }
+  }
+
+  private advanceIterator(): void {
+    this.iterator = (this.iterator + 1) % this.pages.length;
+  }
+
+  private currentPage(): Page {
+    return this.pages[this.iterator];
+  }
+
+  private insert(reference: number) {
+    this.pages[this.iterator] = new Page(reference, 0);
+    this.advanceIterator();
+  }
+}
+
 export function clock(n: number, referenceList: number[]): number[] {
   if (n === 0) return [];
 
-  class Page {
-    constructor(public id: number, public referenceCounter: number) {}
-  }
+  let memory: Memory = new Memory(n);
 
-  class Memory {
-    private readonly pages: Page[] = [];
-    private iterator: number = 0;
+  referenceList.map(memory.request.bind(memory));
 
-    constructor(n: number) {
-      this.pages = Array(n).fill(new Page(-1, 0));
-    }
-
-    private advanceIterator(): void {
-      this.iterator = (this.iterator + 1) % n;
-    }
-
-    public find(reference: number) {
-      return this.pages.find((page) => page.id === reference);
-    }
-
-    public currentPage(): Page {
-      return this.pages[this.iterator];
-    }
-
-    public get pageIds(): number[] {
-      return this.pages.map((page) => page.id);
-    }
-
-    public request(pageReference: number) {
-      let page = this.find(pageReference);
-      if (page) {
-        page.referenceCounter++;
-        return;
-      }
-
-      this.findInsertPosition();
-
-      this.insert(pageReference);
-    }
-
-    private findInsertPosition() {
-      if (this.currentPage().referenceCounter > 0) {
-        this.currentPage().referenceCounter--;
-        this.advanceIterator();
-        this.findInsertPosition();
-      }
-    }
-
-    private insert(reference: number) {
-      this.pages[this.iterator] = new Page(reference, 0);
-      this.advanceIterator();
-    }
-  }
-
-  let alternativeMemory: Memory = new Memory(n);
-
-  for (let reference of referenceList) {
-    alternativeMemory.request(reference);
-  }
-
-  return alternativeMemory.pageIds;
+  return memory.pageIds;
 }
