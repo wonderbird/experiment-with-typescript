717ebf18987d2782dc5f8bf2988eb91052aa9117
Stefan Boos <kontakt@boos.systems>
2023-11-18 16:20:46 +0100

    test(clock2): add codewars tests

--- tests/clock2.test.ts
+++ tests/clock2.test.ts
@@ -1,7 +1,8 @@
 import { clock2 } from "../src/clock2";
 
 import "chai/register-should";
-import { config } from "chai";
+import { assert, config } from "chai";
+import { clock } from "../src/clock";
 config.truncateThreshold = 0;
 
 // Steps of the clock algorithm
@@ -114,5 +115,29 @@ describe("clock2 should", () => {
         clock2(n, referenceList).should.deep.equal(expected);
       }
     );
+  });
+});
+
+function doTest(n: number, referenceList: number[], expected: number[]) {
+  const actual = clock(n, referenceList.slice());
+  assert.deepEqual(
+    actual,
+    expected,
+    `N = ${n}, REFERENCE LIST = [${referenceList}]\n`
+  );
+}
+
+describe("Basic Tests", function () {
+  it("Basic Tests", function () {
+    const TESTS: [number, number[], number[]][] = [
+      [4, [1, 2, 3, 4, 5, 5, 3, 6, 7, 8], [5, 8, 3, 7]],
+      [3, [1, 2, 3, 4, 2, 5], [4, 2, 5]],
+      [4, [1, 2], [1, 2, -1, -1]],
+      [3, [6, 3, 6, 3, 2, 5, 1, 6], [1, 6, 5]],
+      [5, [], [-1, -1, -1, -1, -1]],
+      [1, [1, 2, 3, 4, 5], [5]],
+    ];
+    for (const [n, referencesList, expected] of TESTS)
+      doTest(n, referencesList, expected);
   });
 });
